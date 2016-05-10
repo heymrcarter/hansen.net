2cba95fa0a947240af9ec81a701e32e496ca7239
Tom Carter <heymrcarter14@gmail.com>
2016-05-09 22:47:10 -0400

    removes bootstrap, adds foundation. adds about, contact, and services sections

--- Hansen/App_Start/BundleConfig.cs
+++ Hansen/App_Start/BundleConfig.cs
@@ -19,13 +19,18 @@ namespace Hansen
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/foundation").Include(
+                "~/Scripts/foundation.js"
+                ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/Content/foundation.css",
+                "~/Content/styles/hansen.css"
+            ));
         }
     }
 }
